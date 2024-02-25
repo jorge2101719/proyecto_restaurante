03d1846e77fcf005d8d1814d03430082abd0d58f
jorge2101719 <rusosky01@gmail.com>
2024-02-25 07:11:05 -0300

    formulario básico en login

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,27 +1,49 @@
 import { useContext } from 'react'
 
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-import { RestauranteContext } from '../../context/RestauranteContext'
-import { useNavigate } from 'react-router-dom'
+// import { RestauranteContext } from '../../context/RestauranteContext'
+// import { useNavigate } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Login = () => {
-    const navigateRestaurante = useNavigate()
+    // const navigateRestaurante = useNavigate()
     
 
 
     return (
-        <div>
-            <h1>Login</h1>
-            <form>
-                <label>Ingrese su correo</label>
-                <input type="text" placeholder="Correo" />
+        
+        <Form>
+            <Form.Group>
+                <Form.Label>
+                    <Form.Control
+                        type='email'
+                        id='email'
+                        aria-describedby='emailLogin'
+                        placeholder='email'
+                    />
+                    <Form.Text id='emailLogin'>
+                        Ingrese su correo
+                    </Form.Text>
+                </Form.Label>
+            </Form.Group>
 
-                <Button>Ingresar</Button>
-            </form>
-        </div>
+            <Form.Group>
+                <Form.Label>
+                    <Form.Control
+                        type='password'
+                        id='inputPassword'
+                        aria-describedby='passwordLogin'
+                        placeholder='password'
+                    />
+                    <Form.Text id='passwordLogin'>
+                        Ingrese su clave
+                    </Form.Text>
+                </Form.Label>
+            </Form.Group>
+        </Form>
     )
 }
 

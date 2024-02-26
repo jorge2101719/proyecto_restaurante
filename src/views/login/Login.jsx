92987af0664207ed906098f7bedf0dbe9aad5ea7
jorge2101719 <rusosky01@gmail.com>
2024-02-26 03:56:14 -0300

    creación de formularios básicos para login y registro

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,49 +1,58 @@
-import { useContext } from 'react'
-
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
-// import { RestauranteContext } from '../../context/RestauranteContext'
-// import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Login = () => {
-    // const navigateRestaurante = useNavigate()
-    
+    // Estados del formulario
+    const [correo, setCorreo] = useState('')
+    const [password, setPassword] = useState('')
+    // Estado de errores
+    const [error, setError] = useState(false)
 
+    // Función antes de enviar
+    const validarRegistro = (e) => {
+        e.preventDefault()
+
+        if (correo === '' || password === '') {
+            alert('Debe completar todos los campos')
+            setError(true)
+            return
+        }
+        setError(false)
+        setCorreo('')
+        setPassword('')
+    }
 
     return (
-        
-        <Form>
-            <Form.Group>
-                <Form.Label>
-                    <Form.Control
-                        type='email'
-                        id='email'
-                        aria-describedby='emailLogin'
-                        placeholder='email'
-                    />
-                    <Form.Text id='emailLogin'>
-                        Ingrese su correo
-                    </Form.Text>
-                </Form.Label>
-            </Form.Group>
+        <>
+        <h1>Ingrese sus datos</h1>
 
-            <Form.Group>
-                <Form.Label>
-                    <Form.Control
-                        type='password'
-                        id='inputPassword'
-                        aria-describedby='passwordLogin'
-                        placeholder='password'
-                    />
-                    <Form.Text id='passwordLogin'>
-                        Ingrese su clave
-                    </Form.Text>
-                </Form.Label>
-            </Form.Group>
-        </Form>
+        <form onSubmit={validarRegistro}>
+            <div className='form-group'>
+                <label>Email</label>
+                <input
+                    className='form-control'
+                    name='correo'
+                    type='email'
+                    placeholder='ingrese su correo'
+                    onChange={ e => setCorreo(e.target.value) }
+                    value={correo}
+                />
+            </div>
+            <div className='form-group'>
+                <label>Password</label>
+                <input
+                    className='form-control'
+                    type='password'
+                    name='password'
+                    placeholder='password'
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                />
+            </div>
+            <button type='submit' className='btn btn-primary'>Enviar</button>
+        </form>
+        </>
     )
 }
 
